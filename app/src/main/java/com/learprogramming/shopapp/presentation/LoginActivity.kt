123f388aa188b5b53aa319fbd3a5b8549036657c
tjeong <k.jts8257@gmail.com>
2022-03-10 19:01:03 +0900

    init

---
+++ app/src/main/java/com/learprogramming/shopapp/presentation/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.learprogramming.shopapp.presentation
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.learprogramming.shopapp.R
+import kotlinx.android.synthetic.main.activity_login.*
+
+/**
+ * Login Screen of the application.
+ */
+@Suppress("DEPRECATION")
+class LoginActivity : AppCompatActivity() {
+
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_login)
+
+        // This is used to hide the status bar and make the login screen as a full screen activity.
+        // It is deprecated in the API level 30. I will update you with the alternate solution soon.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        tv_register.setOnClickListener {
+
+            // Launch the register screen when the user clicks on the text.
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
